2ccf72d1a2e883267d825179ec6dbac9c1314f3a
marufmahadyarnob <137247420+marufmahadyarnob@users.noreply.github.com>
2026-02-01 15:22:22 +0600

    Create templates.js

---
+++ js/templates.js
@@ -0,0 +1,12 @@
+const TEMPLATES=[
+{id:"maruf",name:"Maruf News",image:"templates/template-maruf.jpg",frame:{x:190,y:230,w:700,h:700}},
+{id:"mahady",name:"Mahady News",image:"templates/template-mahady.jpg",frame:{x:170,y:220,w:740,h:740}},
+{id:"t1",name:"Breaking News 1",image:"templates/template-1.jpg",frame:{x:200,y:240,w:680,h:680}},
+{id:"t2",name:"Breaking News 2",image:"templates/template-2.jpg",frame:{x:210,y:230,w:660,h:660}},
+{id:"t3",name:"Breaking News 3",image:"templates/template-3.jpg",frame:{x:180,y:250,w:700,h:700}},
+{id:"t4",name:"Breaking News 4",image:"templates/template-4.jpg",frame:{x:190,y:240,w:680,h:680}},
+{id:"t5",name:"Breaking News 5",image:"templates/template-5.jpg",frame:{x:200,y:260,w:700,h:700}},
+{id:"t6",name:"Breaking News 6",image:"templates/template-6.jpg",frame:{x:180,y:220,w:720,h:720}},
+{id:"t7",name:"Breaking News 7",image:"templates/template-7.jpg",frame:{x:190,y:230,w:700,h:700}},
+{id:"t8",name:"Breaking News 8",image:"templates/template-8.jpg",frame:{x:200,y:240,w:680,h:680}}
+];
