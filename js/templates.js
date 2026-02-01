fae0e283e4646f9fcc1f64f148f636cc1748668d
marufmahadyarnob <137247420+marufmahadyarnob@users.noreply.github.com>
2026-02-01 18:41:58 +0600

    Update templates.js

--- js/templates.js
+++ js/templates.js
@@ -1,5 +1,5 @@
 const TEMPLATES=[
-{id:"maruf",name:"Maruf News",image:"templates/template-maruf.jpg",frame:{x:190,y:230,w:700,h:700}},
+{id:"maruf",name:"Maruf News",image:"templates/template-maruf.png",frame:{x:190,y:230,w:700,h:700}},
 {id:"mahady",name:"Mahady News",image:"templates/template-mahady.jpg",frame:{x:170,y:220,w:740,h:740}},
 {id:"t1",name:"Breaking News 1",image:"templates/template-1.jpg",frame:{x:200,y:240,w:680,h:680}},
 {id:"t2",name:"Breaking News 2",image:"templates/template-2.jpg",frame:{x:210,y:230,w:660,h:660}},
